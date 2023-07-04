c61d09f31866929f007115266c8604f9ebeb8871
Rafael R. de Oliveira <rafael.roberto200618@gmail.com>
2023-07-04 14:28:54 -0300

    start and finish drop-down list

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
+// MENU
 const menu = document.querySelector('.menu');
 const overlay = document.querySelector('#overlay');
 const openBtn = document.querySelector('.open-menu');
 const closeBtn = document.querySelector('.close-menu');
 
-/* DESKTOP */
-const featuresDesk = document.querySelector('.features-desk');
-const companyDesk = document.querySelector('.company-desk');
-const btnFeaturesDesk = document.querySelector('.btn-features--desk');
-const btnCompanyDesk = document.querySelector('.btn-company--desk');
-
-/* MOBILE */
-const featuresMobile = document.querySelector('.features-mobile');
-const companyMobile = document.querySelector('.company-mobile');
-const btnFeaturesMobile = document.querySelector('.btn-features--mobile');
-const btnCompanyMobile = document.querySelector('.btn-company--mobile');
-
-function openMenu(){
-    // MENU
+openBtn.addEventListener('click', e => {
     menu.classList.add('menu-open');
     overlay.style.display = 'block';
 
     document.body.style.overflow = 'hidden';
-}
+});
 
 function closeMenu(){
     // MENU
@@ -31,40 +19,52 @@ function closeMenu(){
     document.body.style.overflow = 'auto';
 }
 
-function openFeaturesDesk(){
-    featuresDesk.classList.toggle('list-expanded--open');
-
-    btnFeaturesDesk.classList.toggle('generic-button--active');
-    // addActiveColorToBtnArrow();
-}
-
-function openCompanyDesk(){
-    companyDesk.classList.toggle('list-expanded--open');
-
-    btnCompanyDesk.classList.toggle('generic-button--active');
-    // addActiveColorToBtnArrow();
-}
-
-function openFeaturesMobile(){
-    featuresMobile.classList.toggle('list-expanded--open');
-
-    btnFeaturesMobile.classList.toggle('generic-button--active');
-    // addActiveColorToBtnArrow();
-}
-
-function openCompanyMobile(){
-    companyMobile.classList.toggle('list-expanded--open');
-
-    btnCompanyMobile.classList.toggle('generic-button--active');
-    // addActiveColorToBtnArrow();
-}
-
-openBtn.addEventListener('click', openMenu)
 closeBtn.addEventListener('click', closeMenu);
 overlay.addEventListener('click', closeMenu);
 
-btnFeaturesDesk.addEventListener('click', openFeaturesDesk);
-btnCompanyDesk.addEventListener('click', openCompanyDesk);
+// DROPDOWN MOBILE
+// DROPDOWN MOBILE FEATURES
+const btnFeaturesMobile = document.querySelector('#btn-features--mobile');
+const featuresMobile = document.querySelector('#features-mobile');
+const featuresArrowMobile = document.querySelector('#btn-features--mobile > .arrow-button');
+const featuresSvgMobile = document.querySelector('#btn-features--mobile > .arrow-button > svg > path');
+// DROPDOWN MOBILE COMPANY
+const btnCompanyMobile = document.querySelector('#btn-company--mobile');
+const companyMobile = document.querySelector('#company-mobile');
+const companyArrowMobile = document.querySelector('#btn-company--mobile > .arrow-button');
+const companySvgMobile = document.querySelector('#btn-company--mobile > .arrow-button > svg > path');
 
-btnFeaturesMobile.addEventListener('click', openFeaturesMobile);
-btnCompanyMobile.addEventListener('click', openCompanyMobile);
+// DROPDOWN DESKTOP
+// DROPDOWN DESKTOP FEATURES
+const btnFeaturesDesktop = document.querySelector('#btn-features--desktop');
+const featuresDesktop = document.querySelector('#features-desktop');
+const featuresArrowDesktop = document.querySelector('#btn-features--desktop > .arrow-button');
+const featuresSvgDesktop = document.querySelector('#btn-features--desktop > .arrow-button > svg > path');
+// DROPDOWN DESKTOP COMPANY
+const btnCompanyDesktop = document.querySelector('#btn-company--desktop');
+const companyDesktop = document.querySelector('#company-desktop');
+const companyArrowDesktop = document.querySelector('#btn-company--desktop > .arrow-button');
+const companySvgDesktop = document.querySelector('#btn-company--desktop > .arrow-button > svg > path');
+
+function openDropdown(dropdown, btn, arrow, svg){
+    dropdown.classList.toggle('dropdown-open');
+    btn.classList.toggle('btn-dropdown--active');
+    arrow.classList.toggle('arrow-rotate');
+    svg.classList.toggle('svg-arrow--active');
+}
+
+btnFeaturesMobile.addEventListener('click', e => {
+    openDropdown(featuresMobile, btnFeaturesMobile, featuresArrowMobile, featuresSvgMobile);
+});
+
+btnCompanyMobile.addEventListener('click', e => {
+    openDropdown(companyMobile, btnCompanyMobile, companyArrowMobile, companySvgMobile);
+});
+
+btnFeaturesDesktop.addEventListener('click', e => {
+    openDropdown(featuresDesktop, btnFeaturesDesktop, featuresArrowDesktop, featuresSvgDesktop);
+});
+
+btnCompanyDesktop.addEventListener('click', e => {
+    openDropdown(companyDesktop, btnCompanyDesktop, companyArrowDesktop, companySvgDesktop);
+});
