b19afd0236ccd504334ddbe02e30c37e5ba4abe0
Rafael R. de Oliveira <rafael.roberto200618@gmail.com>
2023-08-09 17:30:34 -0300

    refactor the html, some css and js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // MENU
 const menu = document.querySelector('.menu');
 const overlay = document.querySelector('#overlay');
-const openBtn = document.querySelector('.open-menu');
-const closeBtn = document.querySelector('.close-menu');
+const btnOpen = document.querySelector('.open-menu');
+const btnClose = document.querySelector('.close-menu');
 
-openBtn.addEventListener('click', e => {
+btnOpen.addEventListener('click', () => {
     menu.classList.add('menu-open');
     overlay.style.display = 'block';
 
@@ -19,52 +19,22 @@ function closeMenu(){
     document.body.style.overflow = 'auto';
 }
 
-closeBtn.addEventListener('click', closeMenu);
+btnClose.addEventListener('click', closeMenu);
 overlay.addEventListener('click', closeMenu);
 
-// DROPDOWN MOBILE
-// DROPDOWN MOBILE FEATURES
-const btnFeaturesMobile = document.querySelector('#btn-features--mobile');
-const featuresMobile = document.querySelector('#features-mobile');
-const featuresArrowMobile = document.querySelector('#btn-features--mobile > .arrow-button');
-const featuresSvgMobile = document.querySelector('#btn-features--mobile > .arrow-button > svg > path');
-// DROPDOWN MOBILE COMPANY
-const btnCompanyMobile = document.querySelector('#btn-company--mobile');
-const companyMobile = document.querySelector('#company-mobile');
-const companyArrowMobile = document.querySelector('#btn-company--mobile > .arrow-button');
-const companySvgMobile = document.querySelector('#btn-company--mobile > .arrow-button > svg > path');
+const buttonsDropdown = document.querySelectorAll('[data-btn]');
 
-// DROPDOWN DESKTOP
-// DROPDOWN DESKTOP FEATURES
-const btnFeaturesDesktop = document.querySelector('#btn-features--desktop');
-const featuresDesktop = document.querySelector('#features-desktop');
-const featuresArrowDesktop = document.querySelector('#btn-features--desktop > .arrow-button');
-const featuresSvgDesktop = document.querySelector('#btn-features--desktop > .arrow-button > svg > path');
-// DROPDOWN DESKTOP COMPANY
-const btnCompanyDesktop = document.querySelector('#btn-company--desktop');
-const companyDesktop = document.querySelector('#company-desktop');
-const companyArrowDesktop = document.querySelector('#btn-company--desktop > .arrow-button');
-const companySvgDesktop = document.querySelector('#btn-company--desktop > .arrow-button > svg > path');
+buttonsDropdown.forEach(button => {
+    const attributeValue = button.dataset.btn;
 
-function openDropdown(dropdown, btn, arrow, svg){
-    dropdown.classList.toggle('dropdown-open');
-    btn.classList.toggle('btn-dropdown--active');
-    arrow.classList.toggle('arrow-rotate');
-    svg.classList.toggle('svg-arrow--active');
+    button.addEventListener('click', () => {
+        toggleDropdown(attributeValue);
+        button.classList.toggle('btn-dropdown--active');
+    });
+});
+
+function toggleDropdown(dropdownType){
+    document.querySelector(`[data-dropdown="${dropdownType}"]`).classList.toggle('dropdown-open');
+    document.querySelector(`[data-arrow="${dropdownType}"]`).classList.toggle('arrow-rotate');
+    document.querySelector(`[data-svg="${dropdownType}"]`).classList.toggle('svg-arrow--active');
 }
-
-btnFeaturesMobile.addEventListener('click', e => {
-    openDropdown(featuresMobile, btnFeaturesMobile, featuresArrowMobile, featuresSvgMobile);
-});
-
-btnCompanyMobile.addEventListener('click', e => {
-    openDropdown(companyMobile, btnCompanyMobile, companyArrowMobile, companySvgMobile);
-});
-
-btnFeaturesDesktop.addEventListener('click', e => {
-    openDropdown(featuresDesktop, btnFeaturesDesktop, featuresArrowDesktop, featuresSvgDesktop);
-});
-
-btnCompanyDesktop.addEventListener('click', e => {
-    openDropdown(companyDesktop, btnCompanyDesktop, companyArrowDesktop, companySvgDesktop);
-});
